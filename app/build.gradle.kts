0fb519108b3390fa0e3b3f2bdb1e2f3e2fa1bd28
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2025-04-06 23:29:21 +0300

    add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -16,6 +16,7 @@ repositories {
 }
 
 dependencies {
+    implementation("info.picocli:picocli:4.7.6")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
