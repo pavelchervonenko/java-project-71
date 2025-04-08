b667366519e7ae13d88a23180b750b4957966c28
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2025-04-08 20:19:49 +0300

    add dependencies

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,6 +17,7 @@ repositories {
 
 dependencies {
     implementation("info.picocli:picocli:4.7.6")
+    implementation("com.fasterxml.jackson.core:jackson-databind:2.18.3")
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
