2a169d202c427b4a46a1221b9c10deb85da71cec
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2025-06-06 19:16:55 +0300

    add yml for sonar and update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -1,7 +1,9 @@
 plugins {
     id("java")
     id("com.github.ben-manes.versions") version "0.52.0"
-    application
+    id("checkstyle")
+    id("application")
+    id("org.sonarqube") version "6.2.0.5505"
 }
 
 group = "hexlet.code"
@@ -21,6 +23,19 @@ dependencies {
     testImplementation(platform("org.junit:junit-bom:5.10.0"))
     testImplementation("org.junit.jupiter:junit-jupiter")
 }
+
+checkstyle {
+    toolVersion = "10.12.4"
+    configFile = file("${rootDir}/config/checkstyle/checkstyle.xml")
+    isIgnoreFailures = false
+}
+
+sonar {
+    properties {
+        property("sonar.projectKey", "pavelchervonenko_java-project-71")
+        property("sonar.organization", "pavelchervonenko")
+        property("sonar.host.url", "https://sonarcloud.io")
+    }
 
 tasks.test {
     useJUnitPlatform()
