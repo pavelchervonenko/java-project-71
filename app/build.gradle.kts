2678149694c0b7edd4f1f7694c931ebe44ff8167
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2025-06-07 21:33:32 +0300

    update Makefile and build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -45,8 +45,8 @@ jacoco {
 
 tasks.jacocoTestReport {
     reports {
-        xml.required = true
-        html.required = true
+        xml.required.set(true)
+        html.required.set(true)
     }
 }
 
