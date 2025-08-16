fdf993e23e0dd67633339452ab62928e6e371336
pavelchervonenko <pavel.chervonenko.97@mail.ru>
2025-08-16 21:40:38 +0300

    refactoring

---
+++ app/src/main/java/hexlet/code/FormatterFactory.java
@@ -0,0 +1,16 @@
+package hexlet.code;
+
+import hexlet.code.formatters.JsonFormatter;
+import hexlet.code.formatters.PlainFormatter;
+import hexlet.code.formatters.StylishFormatter;
+
+public class FormatterFactory {
+    public static Formatter get(String name) {
+        return switch (name.toLowerCase()) {
+            case "stylish", "" -> new StylishFormatter();
+            case "plain" -> new PlainFormatter();
+            case "json" -> new JsonFormatter();
+            default -> throw new RuntimeException("Unknown format");
+        };
+    }
+}
